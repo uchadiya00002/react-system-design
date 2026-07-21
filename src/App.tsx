dffccf191a5dc600671bd4ce306a99a63452b918
uchadiya00002 <avinashuchadiya786@gmail.com>
2026-07-21 16:34:51 +0530

    day-2: tab component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import React from "react";
-import Accordion from "./components/easy/Accordion";
-import { faq } from "./utils";
+import React, { useState } from "react";
+import Tabs from "./components/easy/Tabs";
+import Profile from "./components/easy/Tabs/Profile";
+import Setting from "./components/easy/Tabs/Setting";
+// import { faq } from "./utils";
+// import Accordion from "./components/easy/Accordion";
 import "./App.css";
 
+const tabsData = [
+  { label: "Profile", content: <Profile /> },
+  { label: "Settings", content: <Setting /> },
+  { label: "Tab 3", content: "Content for Tab 3" },
+];
+
 function App() {
-  const [activeIndex, setActiveIndex] = React.useState(new Set([]));
+  // const [activeIndex, setActiveIndex] = React.useState(new Set([]));
+  const [currentTab, setCurrentTab] = useState(0);
   return (
     <>
+    <div className="header-label">
       System Design
-      <div>
-        <Accordion
+    </div>
+      {/* <Accordion
           items={faq}
           defaultActiveIndex={0}
           activeIndex={activeIndex}
           onChange={setActiveIndex}
-        />
-      </div>
+        /> */}
+      <Tabs
+        tabs={tabsData}
+        defaultActiveTab={0}
+        currentTab={currentTab}
+        onTabChange={setCurrentTab}
+      />
     </>
   );
 }
